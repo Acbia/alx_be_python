b4923c76b0327f8ec76408b8ad97cde6668b710a
Acbia <nebadave000@gmail.com>
2025-11-16 14:09:11 +0300

    Add pattern_drawing.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,8 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     product = number * i
+<<<<<<< HEAD
     print(f"{number} * {i} = {product}")
+=======
+    print(f"{number} * {i} = {product}")
+>>>>>>> 0c4226a (Add pattern_drawing.py)
