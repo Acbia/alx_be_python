3b7ec2adda2948dc780534a0b4631acf3523f39d
Acbia <nebadave000@gmail.com>
2025-11-23 12:17:21 +0300

    Add arithmetic_operations.py and main.py

---
+++ control-flow/main.py
@@ -0,0 +1,10 @@
+import calculator
+
+# Perform arithmetic operations
+num1 = 5
+num2 = 3
+
+print(f"The sum of {num1} and {num2} is: {calculator.add(num1, num2)}")
+print(f"The difference of {num1} and {num2} is: {calculator.subtract(num1, num2)}")
+print(f"The product of {num1} and {num2} is: {calculator.multiply(num1, num2)}")
+print(f"The division of {num1} by {num2} is: {calculator.divide(num1, num2)}")
